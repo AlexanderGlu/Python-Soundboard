9ba2dda79a64264c3c8766fbe987a45332c600ad
AlexanderGlu <alexandergluesing@posteo.de>
2019-03-03 14:47:57 +0100

    FIX Shortcutüberwachung und abspielen per Rechtsklick Sowie nutzen von ttk buttons etc - soll angepasst aussehen

--- pysb.py
+++ pysb.py
@@ -29,6 +29,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import ttk
 
 import os
 import configparser
@@ -38,6 +39,18 @@ import vlc3 as vlc
 konfigurationsdatei = 'PYSB.ini'
 hk = []
 
+# ######################   [ Vlc player - ini?! ] ########################################
+# Den Vlc Player Initialisieren (für nur eine Instanz)
+# ----------------------------------------------------
+# TODO evtl. mehrere Instanzen ermöglichen, die parallel gestartet werden mit einer Ansicht was gerade geladen ist?
+instanz_vlc = vlc.Instance()
+player = instanz_vlc.media_player_new()
+
+
+# set the volume slider to the current volume
+# volslider.set(player.audio_get_volume())
+
+
 # ----------------- Tk- Dialog --------------------
 
 
@@ -46,11 +59,11 @@ class VolumeErkennenDialog:
         top = self.top = Toplevel(parent)
         top.title("Lautstärke angeben")
 
-        self.slider_senkr = Scale(top, command=self.volume_set, from_=100, to=0, orient=VERTICAL, length=100)
+        self.slider_senkr = ttk.Scale(top, command=self.volume_set, from_=100, to=0, orient=VERTICAL, length=100)
         self.slider_senkr.pack(side=TOP)
-        self.volentry = Entry(top, text=str(root.einzelvol), width=5)
+        self.volentry = ttk.Entry(top, text=str(root.einzelvol), width=5)
         self.volentry.pack(side=TOP)
-        self.buttonexit = Button(top, text="ok", command=self.buttonexitaction)
+        self.buttonexit = ttk.Button(top, text="ok", command=self.buttonexitaction)
         self.buttonexit.pack(side=BOTTOM)
         self.volentry.delete(0, END)
         self.volentry.insert(0, str(root.einzelvol))
@@ -82,9 +95,9 @@ class VolumeErkennenDialog:
             self.volentry.insert(0, self.slider_senkr.get())
 
     def volume_set(self, vol):
-        root.einzelvol = int(vol)
+        root.einzelvol = int(float(vol))
         self.volentry.delete(0, END)
-        self.volentry.insert(0, str(vol))
+        self.volentry.insert(0, str(int(float(vol))))
 
     def quit(self):
         self.top.destroy()
@@ -98,14 +111,14 @@ class HotkeyErkennenDialog:
         self.frameLabel = Frame(top)
         self.frameLabel.pack(pady=5, padx=5)
 
-        self.l1 = Label(self.frameLabel, text="Erkannter Shortcut: ")
+        self.l1 = ttk.Label(self.frameLabel, text="Erkannter Shortcut: ")
         self.l1.pack(side=LEFT)
-        self.e = Entry(self.frameLabel)
+        self.e = ttk.Entry(self.frameLabel)
         self.e.pack(side=RIGHT, padx=5)
 
-        self.b = Button(top, text="OK", command=self.ok)
+        self.b = ttk.Button(top, text="OK", command=self.ok)
         self.b.pack(side=LEFT, pady=5)
-        self.b2 = Button(top, text="Hotkey erkennen", command=self.recordkey)
+        self.b2 = ttk.Button(top, text="Hotkey erkennen", command=self.recordkey)
         self.b2.pack(side=RIGHT, pady=5)
 
     def ok(self):
@@ -129,10 +142,10 @@ def play(hkvar):
         hotkeypressed = keyboard.get_hotkey_name()
         for i in hkvar:
             if i[1] == hotkeypressed:
-                media = root.instanz_vlc.media_new(i[0])
-                root.player.set_media(media)
+                media = instanz_vlc.media_new(i[0])
+                player.set_media(media)
                 volumesetplayer(i[2])
-                root.player.play()
+                player.play()
     except:
         print("Variable " + hotkeypressed +
               " wurde nicht gefunden oder es ist etwas in der Funktion 'def play(hk) schiefgelaufen'")
@@ -143,18 +156,18 @@ def startlisten(hkvar):
         return
     else:
         for i in hkvar:
-            root.listenkeyeventhandlerliste.append(keyboard.add_hotkey(i[1], lambda: play(hk)))
+            root.listenkeyeventhandlerliste.append(keyboard.add_hotkey(i[1], lambda: play(hkvar)))
 
 
 def stoplisten():
     for i in root.listenkeyeventhandlerliste:
         keyboard.remove_hotkey(i)
-    root.player.stop()
+    player.stop()
     root.listenkeyeventhandlerliste = []
 
 
 def volumeset(event):
-    volume = int(event)
+    volume = int(float(event))
     if volume > 100:
         volume = 100
     root.mastervolume = volume
@@ -171,12 +184,12 @@ def calcvolume(einzelvolume, mastervolume):
 
 
 def volumesetplayer(einzelvolume):
-    if root.player is None:
-        return
+    #if player is None:
+    #    return
     volume = int(calcvolume(einzelvolume, root.mastervolume))
     if volume > 100:
         volume = 100
-    if root.player.audio_set_volume(volume) == -1:
+    if player.audio_set_volume(volume) == -1:
         print('Konnte die Lautstärke nicht anpassen')
 
 
@@ -284,11 +297,11 @@ def listenrechtsklick(y):
 
 def listenrechtsklick_play(file):
     if file[0] != "Neuer Eintrag...":
-        media = root.instanz_vlc.media_new(file[0])
+        media = instanz_vlc.media_new(file[0])
         volumesetplayer(file[1])
         root.aktuelles_einzelvolume = file[1]
-        root.player.set_media(media)
-        root.player.play()
+        player.set_media(media)
+        player.play()
 
 
 def listendoppelklick_hotkey(event):
@@ -392,8 +405,8 @@ def setslider(event):
 
 if __name__ == '__main__':
     root = Tk()
-
     root.title("PySB - Python-Soundboard")
+
     root.hotkeyAufnahme = ""
     root.listenkeyeventhandlerliste = []
     root.einzelvol = 0  # wird verwendet für den Volume-Dialog
@@ -405,10 +418,10 @@ if __name__ == '__main__':
     # ---------------------------------
     frame_saveandreload = Frame(root)
     frame_saveandreload.pack(side=TOP)
-    reloadButton = Button(frame_saveandreload, text="Config neu Laden",
-                          command=lambda: reloadconfiganddisplay(konfigurationsdatei))
-    saveButton = Button(frame_saveandreload, text="Speichern",
-                        command=lambda: writeconfig(konfigurationsdatei, root.hotkeys))
+    reloadButton = ttk.Button(frame_saveandreload, text="Config neu Laden",
+                              command=lambda: reloadconfiganddisplay(konfigurationsdatei))
+    saveButton = ttk.Button(frame_saveandreload, text="Speichern",
+                            command=lambda: writeconfig(konfigurationsdatei, root.hotkeys))
     reloadButton.pack(side=LEFT, padx=5, pady=5)
     saveButton.pack(side=RIGHT, padx=5, pady=5)
 
@@ -423,18 +436,19 @@ if __name__ == '__main__':
     # ------------------------------------
     frame_listen_hk = Frame(frame_listen, width=43)
     frame_listen_hk.pack(side=LEFT, fill=Y)
+
     label_hk = Label(frame_listen_hk, text="Shortcuts:")
     label_hk.pack(side=TOP)
     frame_listen_hk_mitte = Frame(frame_listen_hk)
     frame_listen_hk_mitte.pack(side=TOP, fill=Y, expand=1)
     listbox_hk = Listbox(frame_listen_hk_mitte)
     listbox_hk.pack(side=LEFT, fill=Y, expand=1)
-    scrollbar_hk_ve = Scrollbar(frame_listen_hk_mitte, orient=VERTICAL)
-    scrollbar_hk_ve.pack(side=LEFT, fill=Y)
-    scrollbar_hk_ve.configure(command=listbox_hk.yview)
-    scrollbar_hk_ho = Scrollbar(frame_listen_hk, orient=HORIZONTAL)
-    scrollbar_hk_ho.pack(side=TOP, fill=X)
-    scrollbar_hk_ho.configure(command=listbox_hk.xview)
+    # scrollbar_hk_ve = ttk.Scrollbar(frame_listen_hk_mitte, orient=VERTICAL)
+    # scrollbar_hk_ve.pack(side=LEFT, fill=Y)
+    # scrollbar_hk_ve.configure(command=listbox_hk.yview)
+    # scrollbar_hk_ho = ttk.Scrollbar(frame_listen_hk, orient=HORIZONTAL)
+    # scrollbar_hk_ho.pack(side=TOP, fill=X)
+    # scrollbar_hk_ho.configure(command=listbox_hk.xview)
 
     # Frame mit der Dateinamen Liste
     # ------------------------------
@@ -446,12 +460,12 @@ if __name__ == '__main__':
     frame_listen_dn_mitte.pack(side=TOP, fill=BOTH, expand=1)
     listbox_dn = Listbox(frame_listen_dn_mitte)
     listbox_dn.pack(side=LEFT, fill=BOTH, expand=1)
-    scrollbar_dn_ve = Scrollbar(frame_listen_dn_mitte, orient=VERTICAL)
-    scrollbar_dn_ve.pack(side=LEFT, fill=Y)
-    scrollbar_dn_ve.configure(command=listbox_dn.yview)
-    scrollbar_dn_ho = Scrollbar(frame_listen_dn, orient=HORIZONTAL)
-    scrollbar_dn_ho.pack(side=TOP, fill=X)
-    scrollbar_dn_ho.configure(command=listbox_dn.xview)
+    # scrollbar_dn_ve = ttk.Scrollbar(frame_listen_dn_mitte, orient=VERTICAL)
+    # scrollbar_dn_ve.pack(side=LEFT, fill=Y)
+    # scrollbar_dn_ve.configure(command=listbox_dn.yview)
+    # scrollbar_dn_ho = ttk.Scrollbar(frame_listen_dn, orient=HORIZONTAL)
+    # scrollbar_dn_ho.pack(side=TOP, fill=X)
+    # scrollbar_dn_ho.configure(command=listbox_dn.xview)
 
     # Frame mit der Volume Liste
     # --------------------------
@@ -459,16 +473,16 @@ if __name__ == '__main__':
     frame_liste_volume.pack(side=RIGHT, fill=Y)
     label_vol = Label(frame_liste_volume, text="Vol.")
     label_vol.pack(side=TOP)
-    scrollbar_vol_ve = Scrollbar(frame_liste_volume, orient=VERTICAL)
+    scrollbar_vol_ve = ttk.Scrollbar(frame_liste_volume, orient=VERTICAL)
     scrollbar_vol_ve.pack(side=RIGHT, fill=Y)
     listbox_vol = Listbox(frame_liste_volume, width=7)
     listbox_vol.pack(side=RIGHT, fill=Y, expand=1)
     scrollbar_vol_ve.configure(command=listbox_vol.yview)
 
     label_editinfo = Label(root,
-                           text='(Zum editieren "Doppelklicken"         ' +
-                                'Zum Löschen [entf] drücken     ' +
-                                'Rechtsklick zum abspielen)')
+                               text='(Zum editieren "Doppelklicken"         ' +
+                                    'Zum Löschen [entf] drücken     ' +
+                                    'Rechtsklick zum abspielen)')
     label_editinfo.pack()
 
     # Frame für den allgemeinen Lautstärkeregler
@@ -477,7 +491,7 @@ if __name__ == '__main__':
     frame_volume.pack(fill=X)
     label_volume = Label(frame_volume, text="Lautsärke:")
     label_volume.pack(side=LEFT, padx=10)
-    volslider = Scale(frame_volume, command=volumeset, from_=0, to=100, orient=HORIZONTAL, length=600)
+    volslider = ttk.Scale(frame_volume, command=volumeset, from_=0, to=100, orient=HORIZONTAL, length=600)
     volslider.pack(side=LEFT, fill=X, expand=1)
 
     # Frame für das starten/Stopppen der Überwachung
@@ -486,11 +500,11 @@ if __name__ == '__main__':
     frame_ueberwachung.pack(fill=X)
     label_ueberwachung = Label(frame_ueberwachung, text="Shortcut- Überwachung: ")
     label_ueberwachung.pack(side=LEFT)
-    button_ueberw_start = Button(frame_ueberwachung, text='starten', command=lambda: startlisten(root.hotkeys))
+    button_ueberw_start = ttk.Button(frame_ueberwachung, text='starten', command=lambda: startlisten(root.hotkeys))
     button_ueberw_start.pack(side=LEFT, padx=5, pady=5)
-    button_ueberw_stop = Button(frame_ueberwachung, text='stoppen', command=lambda: stoplisten())
+    button_ueberw_stop = ttk.Button(frame_ueberwachung, text='stoppen', command=lambda: stoplisten())
     button_ueberw_stop.pack(side=LEFT, padx=5, pady=5)
-    button_ueberw_quit = Button(frame_ueberwachung, text='Quit', command=root.quit)
+    button_ueberw_quit = ttk.Button(frame_ueberwachung, text='Quit', command=root.quit)
     button_ueberw_quit.pack(side=RIGHT, padx=5, pady=5)
 
     # Funktionen einbinden
@@ -510,13 +524,6 @@ if __name__ == '__main__':
     # ---------------
     configinlistenladen()
 
-    # ######################   [ Vlc player - ini?! ] ########################################
-    # Den Vlc Player Initialisieren (für nur eine Instanz)
-    # ----------------------------------------------------
-    # TODO evtl. mehrere Instanzen ermöglichen, die parallel gestartet werden mit einer Ansicht was gerade geladen ist?
-    root.instanz_vlc = vlc.Instance()
-    root.player = root.instanz_vlc.media_player_new()
-    # set the volume slider to the current volume
-    volslider.set(root.player.audio_get_volume())
+    volslider.set(root.mastervolume)
 
     root.mainloop()
